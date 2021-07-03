abf7ba69df9204dc719d23604ff65dcefa169e78
Mario <palomares.mario14@gmail.com>
2021-07-03 10:21:30 +0000

    setup.sh Update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 
 #!/bin/sh
 
+IP = $(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '192.168.122')
 #PATH to the directory containing the cloned githubs of Sawtooth and CAPE
 ROOT_PATH=""
 #Dir name of CAPE repository
@@ -34,14 +35,19 @@ then
 	tmux split-window -c "$ROOT_URL$SAWTOOTH" -t ioc:1.0 -v
 	tmux split-window -c "$ROOT_URL$SAWTOOTH" -t ioc:1.3 -v
 	tmux send-keys -t ioc:1.0 "sudo -u sawtooth sawtooth-validator -vv" Enter
-	tmux send-keys -t ioc:1.0 "" Enter #Write user key !CAREFUL¡
+	tmux send-keys -t ioc:1.0 "" Enter #WRITE USER KEY !CAREFUL¡
 	tmux send-keys -t ioc:1.1 "sudo -u sawtooth devmode-engine-rust -vv --connect tcp://localhost:5050" Enter
-	tmux send-keys -t ioc:1.1 "" Enter #Write user key !CAREFUL¡
+	tmux send-keys -t ioc:1.1 "" Enter #WRITE USER KEY !CAREFUL¡
 	tmux send-keys -t ioc:1.2 "sudo -u sawtooth sawtooth-rest-api -vv" Enter
-	tmux send-keys -t ioc:1.2 "" Enter #Write user key !CAREFUL¡
+	tmux send-keys -t ioc:1.2 "" Enter #WRITE USER KEY !CAREFUL¡
+	tmux send-keys -t tfm:1.3 "sudo -u sawtooth pbft-engine -vv --connect tcp://$IP:5050" Enter
+	tmux send-keys -t tfm:1.3 "" Enter #wRITE USER KEY !CAREFUL¡
+	tmux send-keys -t tfm:1.4 "sudo -u sawtooth xo-tp-python -vv" Enter
+	tmux send-keys -t tfm:1.4 "" Enter #WRITE USER KEY !CAREFUL¡ 
 fi
 
 #WORK
+tmux new-window -n "SAWTOOTH_GUI" -C "$ROOT_PATH$SAWTOOTH$SAWTOOTH_GUI"
 tmux new-window -n "WORK"
 
 tmux attach
